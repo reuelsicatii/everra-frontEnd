a6de0bd1b69eb5d31e64c066335c6448afde78b0
Reuel Sicat II <reuel@truelogic.com.ph>
2022-08-06 09:59:14 +0800

    FB Messaging

--- src/App.js
+++ src/App.js
@@ -11,6 +11,45 @@ import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
 
+  // const propTypes = {
+  //   pageId: PropTypes.string.isRequired,
+  //   appId: PropTypes.string.isRequired,
+
+  //   shouldShowDialog: PropTypes.bool,
+  //   htmlRef: PropTypes.string,
+  //   minimized: PropTypes.bool,
+  //   themeColor: PropTypes.string,
+  //   loggedInGreeting: PropTypes.string,
+  //   loggedOutGreeting: PropTypes.string,
+  //   greetingDialogDisplay: PropTypes.oneOf(['show', 'hide', 'fade']),
+  //   greetingDialogDelay: PropTypes.number,
+  //   autoLogAppEvents: PropTypes.bool,
+  //   xfbml: PropTypes.bool,
+  //   version: PropTypes.string,
+  //   language: PropTypes.string,
+  //   debug: PropTypes.bool,
+  //   onCustomerChatDialogShow: PropTypes.func,
+  //   onCustomerChatDialogHide: PropTypes.func,
+  // };
+
+  const defaultProps = {
+    shouldShowDialog: false,
+    htmlRef: undefined,
+    minimized: undefined,
+    themeColor: undefined,
+    loggedInGreeting: undefined,
+    loggedOutGreeting: undefined,
+    greetingDialogDisplay: undefined,
+    greetingDialogDelay: undefined,
+    autoLogAppEvents: true,
+    xfbml: true,
+    version: '2.11',
+    language: 'en_US',
+    debug: false,
+    onCustomerChatDialogShow: undefined,
+    onCustomerChatDialogHide: undefined,
+  };
+
   return (
     <div className="App">
       <Router>
